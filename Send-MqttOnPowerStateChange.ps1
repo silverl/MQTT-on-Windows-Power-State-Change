51de2f50a7debbab782774f7c96774c1edab50e8
Larry Silverman <lsilverman@trackabout.com>
2021-01-19 14:54:38 -0600

    Forgot new script

---
+++ Send-MqttOnPowerStateChange.ps1
@@ -0,0 +1,25 @@
+param(
+    [ValidateSet("sleep","wake")]
+    [string]
+    $State
+)
+
+Add-Type -Path "C:\lib\M2Mqtt.4.3.0.0\lib\net45\M2Mqtt.Net.dll"
+
+$maxretries = 20
+$retries = 0
+while (($retries -lt $maxretries) -and ((Test-NetConnection $ENV:MQTT_HOST -Port $ENV:MQTT_PORT).TcpTestSucceeded -eq $false))
+{
+    Write-Output "waiting..."
+    # Don't need to sleep because a failing Test-NetConnection call takes about two seconds.
+    $retries = $retries + 1
+}
+
+$MqttClient = [uPLibrary.Networking.M2Mqtt.MqttClient]($ENV:MQTT_HOST)
+
+# Connect with username and password.
+$mqttclient.Connect([guid]::NewGuid(), $ENV:MQTT_USERNAME, $ENV:MQTT_PWD) | Out-Null
+
+# Publish a message indicating the state, wake or sleep.
+$MqttClient.Publish("windows/$State", [System.Text.Encoding]::UTF8.GetBytes("1")) | Out-Null
+exit 0
